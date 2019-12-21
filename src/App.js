5fcfc05df5360a2f5cf8880d54618f9b7942699b
rdowd <rjarrettdowd@yahoo.com>
2019-12-20 21:14:14 -0500

    got all components into app.js and working on some styles.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Operator from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Display from "./components/DisplayComponents/Display";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -20,8 +23,18 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Numbers />
-        <Operator />
+        <Display />
+        <div className="buttonsContainer">
+          <div className="specNum">
+            <Specials />
+            <Numbers />
+          </div>
+          <div>
+            <Operator />  
+          </div>
+        </div>
+        
+        
       </div>
     </div>
   );
